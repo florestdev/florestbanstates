daf2b9503e75223878913483384d6781c4951bdf
FlorestDev <kirillborisenko2012@gmail.com>
2025-11-19 18:56:05 +0300

    Create GeoInfo.java

---
+++ src/main/java/ru/florestdev/florestBanStates/GeoInfo.java
@@ -0,0 +1,11 @@
+package ru.florestdev.florestBanStates;
+
+public class GeoInfo {
+    public final String countryCode;
+    public final String region;
+
+    public GeoInfo(String countryCode, String region) {
+        this.countryCode = countryCode;
+        this.region = region;
+    }
+}
