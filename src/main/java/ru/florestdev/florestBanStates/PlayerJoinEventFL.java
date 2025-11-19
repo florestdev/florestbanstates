783e1a4776184c899bafe74ff449852043e97e7b
FlorestDev <kirillborisenko2012@gmail.com>
2025-11-19 18:55:26 +0300

    Update PlayerJoinEventFL.java

--- src/main/java/ru/florestdev/florestBanStates/PlayerJoinEventFL.java
+++ src/main/java/ru/florestdev/florestBanStates/PlayerJoinEventFL.java
@@ -1,30 +1,62 @@
 package ru.florestdev.florestBanStates;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
-
-import java.io.IOException;
 
 public class PlayerJoinEventFL implements Listener {
-    public final Plugin plugin;
-    public final Client client;
-
-    public PlayerJoinEventFL(Plugin plugin, Client client) {
-        this.plugin = plugin;
-        this.client = client;
-    }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        try {
-            boolean result = client.make_req(event.getPlayer().getAddress().getHostName(), event.getPlayer().getName());
-            if (result) {
-                event.getPlayer().kickPlayer(plugin.getConfig().getString("your_country_banned").replace("{user}", event.getPlayer().getName()));
-            }
-        } catch (InterruptedException | IOException e) {
-            // ...
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        FlorestBanStates plugin = FlorestBanStates.getInstance();
+
+        String ip = p.getAddress().getAddress().getHostAddress();
+
+        plugin.getLogger().info("[FBS] Player join: " + p.getName() + " IP=" + ip);
+
+        // whitelist
+        if (plugin.whitelistPlayers.contains(p.getName())) {
+            plugin.getLogger().info("[FBS] " + p.getName() + " is whitelisted. Allowed.");
+            return;
         }
+
+        // async logic
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            GeoInfo info = plugin.getClient().getInfo(ip);
+
+            if (info == null) {
+                plugin.getLogger().warning("[FBS] GeoIP failed for IP " + ip + ". Allowing player.");
+                return;
+            }
+
+            plugin.getLogger().info("[FBS] GeoIP: country=" + info.countryCode +
+                    " region=" + info.region);
+
+            // region check
+            if (plugin.unbannedRegions.contains(info.region)) {
+                plugin.getLogger().info("[FBS] Region " + info.region + " allowed.");
+                return;
+            }
+
+            // country ban
+            if (plugin.bannedCountries.contains(info.countryCode)) {
+
+                String msg = plugin.bannedMessage.replace("{user}", p.getName());
+
+                plugin.getLogger().warning("[FBS] BLOCKING " + p.getName() +
+                        " (" + info.countryCode + ", " + info.region + ")");
+
+                Bukkit.getScheduler().runTask(plugin, () -> {
+                    p.kickPlayer(msg);
+                });
+            } else {
+                plugin.getLogger().info("[FBS] ALLOWED: Country " + info.countryCode);
+            }
+
+        });
     }
 }
+
